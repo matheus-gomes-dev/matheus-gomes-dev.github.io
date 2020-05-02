6d68670b7dbb47c8853daae901fcf422a7b9d548
Matheus <lima.eca@gmail.com>
2020-05-01 23:33:56 -0300

    react build added

---
+++ precache-manifest.4d81aa5e099fefe714fd5a7ebf6bb881.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd1b3fc3fdc64d8d71aab5fd7b284aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d05e731b2ec8ab2198",
+    "url": "/static/css/main.289a7de5.chunk.css"
+  },
+  {
+    "revision": "06d51547403aa1e0c826",
+    "url": "/static/js/2.71fb0f43.chunk.js"
+  },
+  {
+    "revision": "00d05e731b2ec8ab2198",
+    "url": "/static/js/main.b8c48e53.chunk.js"
+  },
+  {
+    "revision": "877e402d3952db6a39a0",
+    "url": "/static/js/runtime-main.41efe427.js"
+  },
+  {
+    "revision": "c73f7ced25fdc0a84a0795165de7a569",
+    "url": "/static/media/99leads.c73f7ced.png"
+  },
+  {
+    "revision": "644c6b265eb28111c497a1aaaebe6071",
+    "url": "/static/media/Painel.644c6b26.pptx"
+  },
+  {
+    "revision": "328fa414704e9eb61aed126dc5899780",
+    "url": "/static/media/Resume.328fa414.pdf"
+  },
+  {
+    "revision": "c83241a1fe0d8d6c3cce44a02680bbbf",
+    "url": "/static/media/demo_genius.c83241a1.gif"
+  },
+  {
+    "revision": "38a5ba6c62694a37a793d005ca77687a",
+    "url": "/static/media/droz.38a5ba6c.jpg"
+  },
+  {
+    "revision": "39ce69d3b91ce0a5f99fa2bf30f7a567",
+    "url": "/static/media/droz.39ce69d3.gif"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/static/media/flag_en.36ab476e.svg"
+  },
+  {
+    "revision": "a102e5631626f71ddbc427003063dc0a",
+    "url": "/static/media/flag_pt.a102e563.svg"
+  },
+  {
+    "revision": "900a6e66a952183e34972b07dd247e4a",
+    "url": "/static/media/github-search.900a6e66.gif"
+  },
+  {
+    "revision": "06050622ed7a5f4fee4e36cbe7c4136d",
+    "url": "/static/media/logo_cnpem.06050622.jpg"
+  },
+  {
+    "revision": "50d19eea23ebb9ea8321b02e31a76e5b",
+    "url": "/static/media/logo_cnpq.50d19eea.jpg"
+  },
+  {
+    "revision": "082c5b3051fa8af5cfdd3710c6f66843",
+    "url": "/static/media/logo_github.082c5b30.png"
+  },
+  {
+    "revision": "bbd0a085f1df8052ff51f3a8b42c243e",
+    "url": "/static/media/logo_totvs.bbd0a085.png"
+  },
+  {
+    "revision": "74b8d6f5b10de1af1a0864d4f85dbd6e",
+    "url": "/static/media/logo_wisewood.74b8d6f5.png"
+  },
+  {
+    "revision": "02f31c34ecd2d0ddadfbbe78d59745c8",
+    "url": "/static/media/nleads_pic_1.02f31c34.png"
+  },
+  {
+    "revision": "86254614cbcb9ed0b9714473ab4a7501",
+    "url": "/static/media/onfo_screen.86254614.png"
+  },
+  {
+    "revision": "ee3c7853433105a46eaa9b566652684f",
+    "url": "/static/media/profile.ee3c7853.jpeg"
+  },
+  {
+    "revision": "705b1c4f3917e1d13779bfc053396037",
+    "url": "/static/media/react_shapes_demo.705b1c4f.gif"
+  },
+  {
+    "revision": "e81e4b0deddb216fd6ec0236acc40c29",
+    "url": "/static/media/simonGame.e81e4b0d.png"
+  },
+  {
+    "revision": "30b150cd489202db131009ac9540cec0",
+    "url": "/static/media/social_media_github.30b150cd.png"
+  },
+  {
+    "revision": "ba94c2dac53b39005951548992f980ff",
+    "url": "/static/media/tronwallet.ba94c2da.png"
+  },
+  {
+    "revision": "60923fc186b01f85e44474b10ce166ec",
+    "url": "/static/media/tronwallet_pic_1.60923fc1.jpeg"
+  }
+]);
